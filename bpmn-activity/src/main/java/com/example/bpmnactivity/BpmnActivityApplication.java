b84df22f9833f3ed6a97d3acfdf7a8861e860a37
长歌行 <wxpro@vip.qq.com>
2020-12-01 13:39:41 +0800

    add:添加activity 代码

--- bpmn-activity/src/main/java/com/example/bpmnactivity/BpmnActivityApplication.java
+++ bpmn-activity/src/main/java/com/example/bpmnactivity/BpmnActivityApplication.java
@@ -1,13 +1,129 @@
 package com.example.bpmnactivity;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Scheduled;
 
-@SpringBootApplication(exclude={org.activiti.spring.boot.SecurityAutoConfiguration.class})
-public class BpmnActivityApplication {
+import org.activiti.api.process.model.ProcessDefinition;
+import org.activiti.api.process.model.ProcessInstance;
+import org.activiti.api.process.model.builders.ProcessPayloadBuilder;
+import org.activiti.api.process.runtime.ProcessRuntime;
+import org.activiti.api.process.runtime.connector.Connector;
+import org.activiti.api.process.runtime.events.ProcessCompletedEvent;
+import org.activiti.api.process.runtime.events.listener.ProcessRuntimeEventListener;
+import org.activiti.api.runtime.shared.query.Page;
+import org.activiti.api.runtime.shared.query.Pageable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+import java.util.Random;
+
+@Slf4j
+@SpringBootApplication()
+public class BpmnActivityApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(BpmnActivityApplication.class, args);
+    }
+
+    @Autowired
+    private ProcessRuntime processRuntime;
+
+    @Autowired
+    private SecurityUtil securityUtil;
+
+    @Override
+    public void run(String... args) {
+        securityUtil.logInAs("system");
+
+        Page<ProcessDefinition> processDefinitionPage = processRuntime.processDefinitions(Pageable.of(0, 10));
+        log.info("> Available Process definitions: " + processDefinitionPage.getTotalItems());
+        for (ProcessDefinition pd : processDefinitionPage.getContent()) {
+            log.info("\t > Process definition: " + pd);
+        }
+
+    }
+
+    @Scheduled(initialDelay = 1000, fixedDelay = 1000)
+    public void processText() {
+
+        securityUtil.logInAs("system");
+
+        String content = pickRandomString();
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yy HH:mm:ss");
+
+        log.info("> Processing content: " + content + " at " + formatter.format(new Date()));
+
+        ProcessInstance processInstance = processRuntime.start(ProcessPayloadBuilder
+                .start()
+                .withProcessDefinitionKey("categorizeProcess")
+                .withName("Processing Content: " + content)
+                .withVariable("content", content)
+                .build());
+        log.info(">>> Created Process Instance: " + processInstance);
+
+
+    }
+
+    @Bean
+    public Connector processTextConnector() {
+        return integrationContext -> {
+            Map<String, Object> inBoundVariables = integrationContext.getInBoundVariables();
+            String contentToProcess = (String) inBoundVariables.get("content");
+            // Logic Here to decide if content is approved or not
+            if (contentToProcess.contains("activiti")) {
+                log.info("> Approving content: " + contentToProcess);
+                integrationContext.addOutBoundVariable("approved",
+                        true);
+            } else {
+                log.info("> Discarding content: " + contentToProcess);
+                integrationContext.addOutBoundVariable("approved",
+                        false);
+            }
+            return integrationContext;
+        };
+    }
+
+    @Bean
+    public Connector tagTextConnector() {
+        return integrationContext -> {
+            String contentToTag = (String) integrationContext.getInBoundVariables().get("content");
+            contentToTag += " :) ";
+            integrationContext.addOutBoundVariable("content",
+                    contentToTag);
+            log.info("Final Content: " + contentToTag);
+            return integrationContext;
+        };
+    }
+
+    @Bean
+    public Connector discardTextConnector() {
+        return integrationContext -> {
+            String contentToDiscard = (String) integrationContext.getInBoundVariables().get("content");
+            contentToDiscard += " :( ";
+            integrationContext.addOutBoundVariable("content",
+                    contentToDiscard);
+            log.info("Final Content: " + contentToDiscard);
+            return integrationContext;
+        };
+    }
+
+    @Bean
+    public ProcessRuntimeEventListener<ProcessCompletedEvent> processCompletedListener() {
+        return processCompleted -> log.info(">>> Process Completed: '"
+                + processCompleted.getEntity().getName() +
+                "' We can send a notification to the initiator: " + processCompleted.getEntity().getInitiator());
+    }
+
+    private String pickRandomString() {
+        String[] texts = {"hello from london", "Hi there from activiti!", "all good news over here.", "I've tweeted about activiti today.",
+                "other boring projects.", "activiti cloud - Cloud Native Java BPM"};
+        return texts[new Random().nextInt(texts.length)];
     }
 
 }
--- bpmn-activity/src/test/java/com/example/bpmnactivity/BpmnActivityApplicationTests.java
+++ bpmn-activity/src/test/java/com/example/bpmnactivity/BpmnActivityApplicationTests.java
@@ -1,13 +1,12 @@
 package com.example.bpmnactivity;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class BpmnActivityApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
+public class BpmnActivityApplicationTests {
 
 }
